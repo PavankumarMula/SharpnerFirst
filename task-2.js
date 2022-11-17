b02aecb9b264c0da6dc4170a791ee3f9b2735b70
PavankumarMula <pavanpappy7@gmail.com>
2022-11-17 23:53:44 +0530

    Made tasks regarding className

---
+++ task-2.js
@@ -0,0 +1,15 @@
+/* Get Element by Id;
+var title=document.getElementById('header-title');
+title.style.fontWeight='bold'
+title.style.color='yellow';*/
+//get Element By Class name
+/*var additems=document.getElementsByClassName('title');
+additems[0].style.color='brown';*/
+//making 3rd element in the list green background color
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor='green';
+//making all the elements in the list bold font;
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
