04958e99ca99ba0f3393515719933eab9d499cc0
PavankumarMula <pavanpappy7@gmail.com>
2022-11-17 22:24:04 +0530

    Made Changes Item Lister Project

---
+++ basics.js
@@ -0,0 +1,5 @@
+var title=document.getElementById('header-title');
+title.style.border='solid 5px black';
+var additem=document.getElementById('Maintitle')
+additem.style.color='green';
+additem.style.fontWeight='bolder';
