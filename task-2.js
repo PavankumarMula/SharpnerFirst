e3d4af5362223a5a5f2e87c386e47bfa13eac79c
PavankumarMula <pavanpappy7@gmail.com>
2022-11-18 10:04:38 +0530

    changes Saved

--- task-2.js
+++ task-2.js
@@ -6,10 +6,17 @@ title.style.color='yellow';*/
 /*var additems=document.getElementsByClassName('title');
 additems[0].style.color='brown';*/
 //making 3rd element in the list green background color
-var items=document.getElementsByClassName('list-group-item');
+/*var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 items[2].style.backgroundColor='green';
 //making all the elements in the list bold font;
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
-}
+}*/
+
+//get Element by tagname
+var li=document.getElementsByTagName('li');
+li[0].style.color='tomato';
+//get element by class name
+var liclass=document.getElementsByClassName('list-group-item active');
+liclass.color='yellow'
